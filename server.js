0f8de681c28b890f59463af6913039632536fc5f
olichid <genchzc@olichid.com>
2019-03-31 14:52:21 +1100

    add the mongodb connection via mongoose

--- server.js
+++ server.js
@@ -9,6 +9,23 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// configuring the datebase
+const dbConfig = require('./config/database.config');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// connecting to the datebase
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Connected to the MongoDB successfully!")
+}).catch(err => {
+    console.log("Could not connect to the MongoDB, Exiting now...", err)
+    process.exit();
+});
+
+
 // define a simple route
 app.get('/', (req, res) => {
     res.json({"message" : "It's the beginning of a note taking application"})
